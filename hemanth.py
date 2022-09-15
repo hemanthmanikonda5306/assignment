55cee07c7970cb06d412b0a0c7863a17d16eeb33
hemanthmanikonda5306 <113494377+hemanthmanikonda5306@users.noreply.github.com>
2022-09-15 17:20:24 +0530

    new3

---
+++ hemanth.py
@@ -0,0 +1,21 @@
+# ####arithmetic operators#########
+#
+a,b = [eval(x) for x in input('enter two values::').split()]
+#
+# print('addition:', a+b)
+# print('subraction:',a-b)
+# print('multiplication:',a*b)
+# print('divison:',a/b)
+# print('floor divison:', a//b)
+# print('modules:', a%b)
+#
+#comparison operators###
+# print('x>y', a>b)
+# print('x<y', a<b)
+# print('x<=y', a<=b)
+# print('x>=y', a>=b)
+# print('x!=y',a!=b)
+# print('x==y',a==b)
+
+###logical operators#######
+
