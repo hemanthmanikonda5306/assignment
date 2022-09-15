5e247ff67d817718191291b8a95a0bce1a1cc9c9
hemanthmanikonda5306 <113494377+hemanthmanikonda5306@users.noreply.github.com>
2022-09-15 17:07:11 +0530

    new

---
+++ practice1.py
@@ -0,0 +1,54 @@
+# 1st
+# a = 'length of a number'
+# print(len(a))
+# 2nd
+# b = '3'
+# print(type(b))
+# c = int(b)
+# print(c)
+# print(type(c))
+# x = '3.5'
+# print(type(x))
+# y = float(x)
+# print(type(y))
+# 3rd
+# a = [('hemanth', 24), ('kumar', 22)]
+# print(type(a))
+# b = dict(a)
+# print(b)
+# print(type(b))
+# 4th
+# a = ['hemanth', 'kumar', 33, 22.8]
+# b = tuple(a)
+# print(b)
+# print(type(b))
+# print(type(a))
+# c = list(b)
+# print(type(c))
+# 5th
+# a = {'hemanth':24, 'kumar':22, 'manikonda':1998}
+# print(type(a))
+# a.update(hemanth=22,kumar = 24, manikonda = 1999, anjibabu = 22)
+# print(a)
+# 6th
+# a = [2, 34, 2, 2, 2, 34, 22, 34.9, 'hemanth']
+# b = ('hemanth', 'kumar', 55, 'ku')
+# print(a.count(2))
+# print(a.index('hemanth'))
+# print(a.remove(34))
+# print(a.pop())
+# 7th
+# input = 'welcome to python'
+# a = input()
+# print(a[::-1])
+# 8th
+# a = 'hemanth'
+# print(a[::-1])
+# 9th
+# a = [10, 5, 4, 20, 1, 15, 12]
+# a.sort()
+# print(a)
+# 10
+# a = [i for i in list if ]
+
+print([x**2 for x in range(10,20) if x%2 == 0])
